7e478e8be082eda3482d5666dfd088c1d90ffc40
Rikza Fauzan <67640598+rikzafauzannf@users.noreply.github.com>
2024-05-25 09:06:06 +0700

    update padding

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="night">
       <body className={inter.className}>
-        <section className="w-full min-h-dvh p-10 bg-gradient-to-tr from-primary/50 to-secondary/50 space-y-4">
+        <section className="w-full min-h-dvh p-5 bg-gradient-to-tr from-primary/50 to-secondary/50 space-y-4">
           {children}
         </section>
       </body>
